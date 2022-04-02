d7803d3c01f9b8fb2769690880a5c1dce2449002
standfer <vertigl@yandex.ru>
2022-04-02 17:34:35 +0800

    Задача 30. Массив их 0 и 1

---
+++ Programming_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 30. Напишите программу которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+int [] Mass = new int [8];
+Random NewMass = new Random();
+for (int i=0; i<8; i++)
+{
+    Mass [i] = NewMass.Next (0,2);
+    Console.Write($" {Mass[i]}");
+}
