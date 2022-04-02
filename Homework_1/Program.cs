1a55912a1a7eadd6b5b0bd89b52d7c267bcab231
standfer <vertigl@yandex.ru>
2022-04-02 17:41:07 +0800

    ДЗ. Задача 25

---
+++ Homework_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = Convert.ToInt32(Console.ReadLine());
+double c = 1;
+for (int i=0; i < b; i++)
+{
+    c=c*a;
+}
+ Console.WriteLine(c);
