803942a1e28e1ef3631c7e23bf2d3976d5d7fd75
standfer <vertigl@yandex.ru>
2022-04-02 17:22:11 +0800

    Задача 28. Число от 1 до N

---
+++ Programming_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 28. Принимает на вход число N, а выдает произведение чисел от 1 до N
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;   // значение факториала
+
+for (int i = 1; i <= a; i++) 
+{
+     factorial = factorial * i;
+}
+Console.Write(factorial);
