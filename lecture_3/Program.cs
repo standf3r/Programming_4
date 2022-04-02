5584c3fbd68ee23fe3fcebf4e5e0b97c3ef02235
standfer <vertigl@yandex.ru>
2022-04-02 13:12:56 +0800

    Лекция 3

---
+++ lecture_3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+// Method1();
+
+// Вид 2
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2("Текст сообщения");
+
+// Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+// int year = Method3();
+Console.WriteLine(year);
+
+// Вид 4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+    
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+ 
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
