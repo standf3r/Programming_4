95469de1304827d9e3a998c9f84320e7cc66b080
standfer <vertigl@yandex.ru>
2022-04-02 18:12:32 +0800

    ДЗ. Задача 27. Сумма чисел

---
+++ Homework_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27. Напишите программу, коротая принимает на вход число и выдает сумму цифр в числе.
+
+Console.Write("Введите число :");
+string a = Convert.ToString(Console.ReadLine());
+            int summ = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (char.IsNumber(a[i]))
+                {
+ 
+                    summ += Convert.ToInt32(a[i].ToString());
+                }
+            }
+Console.WriteLine($"Сумма числе равнa: {summ}");
+
+
+ 
+
